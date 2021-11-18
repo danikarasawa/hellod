7e74d20ff2c4930a9c13cc645526de2dbb01caf0
dkarasawa <daniela.karasawa@mercadolivre.com>
2021-11-18 19:36:29 -0300

    added test

---
+++ src/test/java/daniela/dev/hellod/test.java
@@ -0,0 +1,66 @@
+// package daniela.dev.hellod;
+
+// import java.util.Scanner;
+// public class test{
+// public static void main(String[] args) {
+//     Scanner key = new Scanner(System.in);
+    
+//     while (key.hasNext()) {
+
+//         String expression = key.nextLine();
+//         int open = 0;
+//         boolean wrong = false;
+        
+//         for (int i = 0; i < expression.length(); i++) {
+//             char caracter = expression.charAt(i);
+//             if (caracter == ')') {
+//                 if (open == 0) {
+//                     wrong = true;
+//                     break;
+//                 }
+//                 open--;
+//             } else if (caracter == '(') {
+//                 open++;
+//             }
+//         }
+//         if (wrong || open > 0) {
+//             System.out.println("incorrect");	
+//         } else {
+//             System.out.println("correct");
+//         }
+//     }
+// }
+// }
+
+// // public class test {
+
+// //     public static void main(String[] args) {
+// //         Scanner key = new Scanner(System.in);
+        
+// //         while (key.hasNext()) {
+
+// // 	        String expression = key.nextLine();
+// // 	        int open = 0;
+// // 	        boolean wrong = false;
+            
+// // 	        for (int i = 0; i < expression.length(); i++) {
+// // 				char caracter = expression.charAt(i);
+// // 				if (caracter == ')') {
+// // 					if (open == 0) {
+// // 						wrong = true;
+// // 						break;
+// // 					}
+// // 					open--;
+// // 				} else if (caracter == '(') {
+// // 					open++;
+// // 				}
+// // 			}
+// //         	if (wrong || open > 0) {
+// // 	            System.out.println("incorrect");	
+// // 	        } else {
+// // 	            System.out.println("correct");
+// // 	        }
+// // 		}
+    	
+// // 	}
+// // }
